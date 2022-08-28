db7e7ab65936e43f6ddd8011dfb4bb3df7f2b3ae
Camilo Galindo R <cgalindorivera@gmail.com>
2022-08-27 22:06:49 -0500

    update link api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import logo from "./assets/logo.png";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://chats-sockets-api.camilog909.repl.co");
 
 interface Message {
 	body: string;
